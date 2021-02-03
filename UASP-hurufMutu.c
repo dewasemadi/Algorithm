ac37df43e167d1f070b8b28f327a5eff6a05ba2c
I Dewa Putu Semadi <dewasemadi@apps.ipb.ac.id>
2021-02-03 08:26:54 +0800

    Update UASP-hurufMutu.c

--- UASP-hurufMutu.c
+++ UASP-hurufMutu.c
@@ -10,27 +10,15 @@ int main(){
 		nilai=(a+b+c)/3.0;
 		
 		if(a>60 && b>60 && c>60 && (a+b+c)/3 > 75)
-		{
 			printf("%.2f A\n", nilai);
-		}
 		else if(a>40 && b>40 && c>40 && (a+b+c)/3 > 60)
-		{
 			printf("%.2f B\n", nilai);
-		}
 		else if(a!=0 && b!=0 && c!=0 && (a+b+c)/3 > 50)
-		{
 			printf("%.2f C\n", nilai);
-		}
 		else if(a!=0 && b!=0 && c!=0  && (a+b+c)/3 > 40)
-		{
 			printf("%.2f D\n", nilai);
-		}
 		else
-		{
 			printf("%.2f E\n", nilai);
-		}
 	}
-	
-	
 	return 0;
 }
